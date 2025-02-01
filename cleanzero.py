1f6188c23358b41d5cb03b95901df4ea809d306b
sheikhspeare <sheikhspeare@localhost.localdomain>
2025-02-02 02:59:10 +0500

    woohoo

---
+++ cleanzero.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ipth', nargs='?')
+
+args = parser.parse_args()
+
+directory = args.ipth or "/content/drive/MyDrive"
+
+
+# Check if the directory exists
+if not os.path.exists(directory):
+    print(f"Error: Directory '{directory}' does not exist.")
+else:
+    # Iterate over all files in the directory
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+    
+        # Check if it's a file, begins with a number, and has a size of 0 bytes
+        if os.path.isfile(filepath) and os.path.getsize(filepath) == 0:
+            os.remove(filepath)  # Delete the file
+            print(f"Deleted: {filepath}")
