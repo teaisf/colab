0799c5897779fd78cf6120abe035eebe6ada4f4d
sheikhspeare <sheikhspeare@localhost.localdomain>
2025-02-15 04:39:24 +0500

    woohoo

---
+++ cleanext.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ipth', nargs='?')
+
+args = parser.parse_args()
+
+root_dir = args.ipth or "/content/drive/MyDrive"
+extens = args.e or ".srt"
+
+# Check if the directory exists
+if not os.path.exists(root_dir):
+    print(f"Error: Directory '{root_dir}' does not exist.")
+else:
+    import os
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for file in filenames:
+            if file.endswith(extens):  # Check for .srt files
+                file_path = os.path.join(dirpath, file)
+                with open(file_path, 'w'): pass
+                os.remove(file_path)  # Delete the file
+                print(f"Deleted: {file_path}")
+
+    print(f"All {extens} files have been deleted.")
